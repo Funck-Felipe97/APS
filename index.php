a4455d1773de743957c1fdc7291e8df94b802af6
Felipe Nazario <nazario@alunos.utfpr.edu.br>
2018-05-11 13:04:01 -0300

    teste

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Principal</title>
+</head>
+<body>
+	<div id="todo">
+		<div id="header">
+			cabeça
+
+		</div>
+		<div id="menu">
+			ombro
+			
+		</div>
+		<div id="conteudo">
+			joelho
+
+		</div>
+		<div id="footer">
+			pé
+
+		</div>
+
+
+	</div>
+</body>
+</html>
